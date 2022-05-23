e8ce2b00e225423325a38c7fdd500b20881e6dbe
Ranidu Harshana <68012743+ranidu-harshana@users.noreply.github.com>
2022-05-24 01:50:11 +0530

    just

--- database/migrations/2022_04_26_082546_create_customers_table.php
+++ database/migrations/2022_04_26_082546_create_customers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('function_type_id');
+            
             $table->string('bill_nulber')->nullable();
             $table->string('name')->nullable();
             $table->string('address')->nullable();
